19b17cba0a0ee70fb50b7afbf293756180c5cbad
Smertig <akaraevz@mail.ru>
2026-04-07 00:53:05 +0300

    bump version to 0.8.5

--- docs/html/demo/cdn.js
+++ docs/html/demo/cdn.js
@@ -1,4 +1,4 @@
-const CDN_PKG = 'https://cdn.jsdelivr.net/npm/autoangel@0.8.4';
+const CDN_PKG = 'https://cdn.jsdelivr.net/npm/autoangel@0.8.5';
 const LOCAL_PKG = '../../../../autoangel-wasm/pkg';
 
 export function resolveCDN(importMetaUrl) {
