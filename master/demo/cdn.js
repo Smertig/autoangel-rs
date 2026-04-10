5c0f60faded7b45cec5e3d91fc4f26baf046c556
Smertig <Smertig@users.noreply.github.com>
2026-04-10 12:52:16 +0000

    deploy: f6281df0ed8f435167ed26a212c9eacd865819ef

--- master/demo/cdn.js
+++ master/demo/cdn.js
@@ -1,5 +1,5 @@
 const CDN_PKG = 'https://cdn.jsdelivr.net/npm/autoangel@0.9.0';
-const LOCAL_PKG = '../../../../autoangel-wasm/pkg';
+const LOCAL_PKG = './autoangel-wasm-pkg';
 
 export function resolveCDN(importMetaUrl) {
   return new URLSearchParams(location.search).has('local')
