5bc178763880c10557a3d0f06a0f835123aef84b
Smertig <Smertig@users.noreply.github.com>
2026-04-29 09:46:44 +0000

    deploy: 8c5bd3d752ca74a10a56afc51923be9ac5b13704

---
+++ master/demo/assets/useNullableGetData-B4etguGn.js
@@ -0,0 +1,1 @@
+import{r as a}from"./useDividerDrag-BTVZTNE3.js";function e(r){return a.useCallback(async t=>{try{return await r(t)}catch{return null}},[r])}export{e as u};
--- master/demo/assets/useNullableGetData-z3XTvD61.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./useDividerDrag-Bp5FcGoj.js";function e(r){return a.useCallback(async t=>{try{return await r(t)}catch{return null}},[r])}export{e as u};
